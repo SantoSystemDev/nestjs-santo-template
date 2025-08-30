40cbb1e9f9a1d494e7df31d4a3317b309b08d3b3
Anderson Santo <12055277+santo-ti@users.noreply.github.com>
2025-08-30 18:39:12 -0300

    refactor(config): update import paths for consistency and fix tsconfig formatting

--- jest.config.ts
+++ jest.config.ts
@@ -1,11 +1,11 @@
-import { Config } from 'jest';
-import { pathsToModuleNameMapper } from 'ts-jest';
 import * as fs from 'fs';
+import type { Config } from 'jest';
 import * as path from 'path';
+import { pathsToModuleNameMapper } from 'ts-jest';
 
 // Carrega o tsconfig.json de forma compatível com as versões mais novas do Jest
 const tsconfig = JSON.parse(
-  fs.readFileSync(path.resolve(__dirname, './tsconfig.json'), 'utf8'),
+  fs.readFileSync(path.resolve(process.cwd(), './tsconfig.json'), 'utf8'),
 );
 
 const config: Config = {
