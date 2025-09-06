278e26824523c51449826df11932656719c5685d
Anderson Santo <12055277+santo-ti@users.noreply.github.com>
2025-09-06 18:01:00 -0300

    refactor(auth): remove unused authentication strategies and related tests

--- src/modules/auth/infrastructure/adapters/credentials/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './jwt-auth.guard';
-export * from './jwt.strategy';
-export * from './password-auth.guard';
-export * from './password.strategy';
--- src/modules/auth/infrastructure/providers/auth.providers.ts
+++
@@ -1,4 +0,0 @@
-import { AuthService } from '@auth/application/services';
-import { Provider } from '@nestjs/common';
-
-export const AuthProviders: Provider[] = [AuthService];
