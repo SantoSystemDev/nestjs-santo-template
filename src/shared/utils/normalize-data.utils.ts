e3c447e33ce10324549ab798f8bc9381f2cb0f10
Anderson Santo <12055277+santo-ti@users.noreply.github.com>
2025-09-06 18:04:20 -0300

    refactor(normalization): update normalization functions to handle optional parameters

--- src/shared/utils/normalize-data.utils.ts
+++ src/shared/utils/normalize-data.utils.ts
@@ -1,13 +1,48 @@
-export const normalizeEmail = (email: string): string => {
-  return email.trim().toLowerCase();
+export const normalizeEmail = (email?: string): string => {
+  return email?.trim().toLowerCase() || '';
 };
 
-export const normalizeFullName = (fullName: string): string => {
-  return fullName.trim().toUpperCase();
+export const normalizeName = (name?: string): string => {
+  return normalizeString(name).toUpperCase() || '';
 };
 
-export const normalizePhoneNumber = (
-  phoneNumber: string | undefined,
-): string | undefined => {
-  return phoneNumber?.trim().replace(/\D/g, '');
+export const normalizePhoneNumber = (phoneNumber?: string): string => {
+  return phoneNumber?.trim().replace(/\D/g, '') || '';
+};
+
+export const normalizeString = (str?: string): string => {
+  return str?.replace(/\s+/g, ' ').trim() || '';
+};
+
+export const normalizeBoolean = (value?: boolean | string): boolean => {
+  if (typeof value === 'string') {
+    return value.toLowerCase() === 'true';
+  }
+  return !!value;
+};
+
+export const normalizeFloatNumber = (value?: number | string): number => {
+  if (typeof value === 'string') {
+    const parsed = parseFloat(value);
+    return isNaN(parsed) ? 0 : parsed;
+  }
+  return value || 0;
+};
+
+export const normalizeIntNumber = (value?: number | string): number => {
+  if (typeof value === 'string') {
+    const parsed = parseInt(value, 10);
+    return isNaN(parsed) ? 0 : parsed;
+  }
+  return value || 0;
+};
+
+export const normalizeUrl = (url?: string): string => {
+  if (!url) return '';
+  try {
+    const normalizedUrl = new URL(url);
+    return normalizedUrl.toString();
+  } catch {
+    return '';
+  }
 };
